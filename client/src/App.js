9da8adf08800dc21c7cad7758a330fe55a02b9a6
Matt Boisseau <boisseaumr@gmail.com>
2020-02-11 02:13:03 -0700

    Connect client and api

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			apiResponse: ''
+		};
+	}
+
+	componentDidMount() {
+		fetch('http://localhost:9000/testAPI')
+			.then(response => response.text())
+			.then(test => this.setState({
+				apiResponse: test
+			}));
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						{ this.state.apiResponse }
+					</p>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
